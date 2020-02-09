c6c9e59679af7949b0c95d69a5d351020f81be01
KangMinseok <qbq5000@gmail.com>
2020-02-09 13:42:20 +0900

    init: <Do Did List> + 'Do Did List' iOS 앱 프로젝트 파일을 생성하였습니다.

---
+++ Do_Did_List/Do_Did_List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Do_Did_List
+//
+//  Created by 강민석 on 2020/02/09.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
