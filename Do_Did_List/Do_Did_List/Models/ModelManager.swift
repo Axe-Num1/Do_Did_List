3fda2444bda98b1e54168088c87056da3e3e665d
KangMinseok <qbq5000@gmail.com>
2020-03-18 23:59:17 +0900

    Add ModelManager

---
+++ Do_Did_List/Do_Did_List/Models/ModelManager.swift
@@ -0,0 +1,14 @@
+//
+//  ModelManager.swift
+//  Do_Did_List
+//
+//  Created by 강민석 on 2020/03/18.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class ModelManager {
+    
+}
