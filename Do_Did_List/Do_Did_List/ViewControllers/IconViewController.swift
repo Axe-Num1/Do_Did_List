8617be2afb142676eb93188bda431189deb921f9
KangMinseok <qbq5000@gmail.com>
2020-03-26 23:59:40 +0900

    Setting IconView

--- Do_Did_List/Do_Did_List/ViewControllers/IconViewController.swift
+++ Do_Did_List/Do_Did_List/ViewControllers/IconViewController.swift
@@ -9,22 +9,17 @@
 import UIKit
 
 class IconViewController: UIViewController {
-
+    
+    @IBOutlet weak var iconImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        isModalInPresentation = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func doneButton(_ sender: Any) {
+        
     }
-    */
-
+    
 }
