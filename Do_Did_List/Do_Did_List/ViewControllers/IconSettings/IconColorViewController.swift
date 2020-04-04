6ab073f0916c2446e8597f98a976d843316289c5
KangMinseok <qbq5000@gmail.com>
2020-04-04 10:46:47 +0900

    Modify colorCollectionView delegate
    + add: Cell Selected Effect
    + modify: collectionView SectionInset

--- Do_Did_List/Do_Did_List/ViewControllers/IconSettings/IconColorViewController.swift
+++ Do_Did_List/Do_Did_List/ViewControllers/IconSettings/IconColorViewController.swift
@@ -23,7 +23,6 @@ class IconColorViewController: UIViewController {
         setCollectionView()
     }
     
-    
     func setCollectionView() {
         
         colorCollectionView.delegate = self
@@ -38,20 +37,13 @@ class IconColorViewController: UIViewController {
             layout.minimumLineSpacing = 5.0
             layout.minimumInteritemSpacing = 5.0
 
-            layout.sectionInset = UIEdgeInsets(top: 10, left: 40, bottom: 0, right: 40)
+            layout.sectionInset = UIEdgeInsets(top: 25, left: 55, bottom: 0, right: 55)
         }
     }
 }
 
-extension IconColorViewController: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard let colors = colorTable[indexPath.row] else { fatalError("Colortable error!") }
-        
-        let colorDataDict:[String: [UIColor]] = ["colorDict": [colors[0], colors[1]]]
-        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "colorRefersh"), object: nil, userInfo: colorDataDict)
-    }
-}
 
+// MARK: - UICollectionViewDataSource
 extension IconColorViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return colorTable.count
@@ -59,10 +51,31 @@ extension IconColorViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ColorCell", for: indexPath)
-        guard let colors = colorTable[indexPath.row] else { fatalError("Colors table error") }
+        guard let colors = colorTable[indexPath.row] else { fatalError("ColorTable error") }
         cell.setGradientBackgroundColor(colorOne: colors[0], colorTow: colors[1])
         cell.roundCorners(.allCorners, radius: (cell.frame.width / 2))
         
         return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension IconColorViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let colors = colorTable[indexPath.row] else { fatalError("ColorTable error!") }
+        
+        let colorDataDict:[String: [UIColor]] = ["colorDict": [colors[0], colors[1]]]
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "colorRefersh"), object: nil, userInfo: colorDataDict)
+        
+        if let cell = collectionView.cellForItem(at: indexPath) {
+            cell.layer.borderColor = UIColor.white.cgColor
+            cell.layer.borderWidth = 2.5
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        if let cell = collectionView.cellForItem(at: indexPath) {
+            cell.layer.borderWidth = 0.0
+        }
     }
 }
