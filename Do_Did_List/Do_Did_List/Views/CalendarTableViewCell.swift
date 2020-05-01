f94dc54dc1dccf02f1e4c2d93484e1ee7c9d571e
KangMinseok <qbq5000@gmail.com>
2020-05-01 23:55:20 +0900

    Add CalendarTableViewCell

---
+++ Do_Did_List/Do_Did_List/Views/CalendarTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CalendarTableViewCell.swift
+//  Do_Did_List
+//
+//  Created by 강민석 on 2020/05/01.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import UIKit
+
+class CalendarTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
